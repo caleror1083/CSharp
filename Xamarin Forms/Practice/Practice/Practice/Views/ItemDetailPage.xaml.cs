18b1f657fe0162708b3c66d8761a66491e8d8ff9
Rob C <caleror1083@gmail.com>
2022-05-24 13:41:10 -0400

    No changes

---
+++ Xamarin Forms/Practice/Practice/Practice/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Practice.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Practice.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
