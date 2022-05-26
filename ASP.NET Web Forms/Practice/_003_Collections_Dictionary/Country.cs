f3305af5fa018029fd66c2cad1f5770287b42f0e
Rob C <caleror1083@gmail.com>
2022-05-25 20:15:14 -0400

    No changes

--- ASP.NET Web Forms/Practice/_003_Collections_Dictionary/Country.cs
+++ ASP.NET Web Forms/Practice/_003_Collections_Dictionary/Country.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace _003_Collections_Dictionary
-{
-	public class Country
+﻿namespace _003_Collections_Dictionary
 	{
-		// Properties
-		public string Name { get; set; }
-		public string Code { get; set; }
-		public string Capital { get; set; }
+		public class Country
+			{
+				// Properties
+				public string Name { get; set; }
+				public string Code { get; set; }
+				public string Capital { get; set; }
+			}
 	}
-}
