3602d4b2ef920b82d1a22226492af1d08cdaef53
Rob C <caleror1083@gmail.com>
2022-05-27 12:34:23 -0400

    Completed WebForm1

--- ASP.NET Web Forms/Practice/_004_Collections_Stack/WebForm1.aspx.cs
+++ ASP.NET Web Forms/Practice/_004_Collections_Stack/WebForm1.aspx.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace _004_Collections_Stack
+﻿namespace _004_Collections_Stack
 	{
-		public partial class WebForm1 : BasePage
-			{
-				protected void Page_Load(object sender, EventArgs e)
-					{
-
-					}
-			}
+		public partial class WebForm1 : BasePage {}
 	}
