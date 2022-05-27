e0482b5640207b10a94f5e11bcf093a2e27bd080
Rob C <caleror1083@gmail.com>
2022-05-27 13:37:37 -0400

    Added Multithreading example

---
+++ Windows Forms Applications/Practice/_001_Multithreading/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _001_Multithreading
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TimeConsumingButton = new System.Windows.Forms.Button();
+			this.PrintButton = new System.Windows.Forms.Button();
+			this.NumbersListbox = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// TimeConsumingButton
+			// 
+			this.TimeConsumingButton.Location = new System.Drawing.Point(26, 33);
+			this.TimeConsumingButton.Name = "TimeConsumingButton";
+			this.TimeConsumingButton.Size = new System.Drawing.Size(195, 23);
+			this.TimeConsumingButton.TabIndex = 0;
+			this.TimeConsumingButton.Text = "Do Time Consuming Work";
+			this.TimeConsumingButton.UseVisualStyleBackColor = true;
+			this.TimeConsumingButton.Click += new System.EventHandler(this.TimeConsumingButton_Click);
+			// 
+			// PrintButton
+			// 
+			this.PrintButton.Location = new System.Drawing.Point(78, 62);
+			this.PrintButton.Name = "PrintButton";
+			this.PrintButton.Size = new System.Drawing.Size(88, 23);
+			this.PrintButton.TabIndex = 1;
+			this.PrintButton.Text = "Print Numbers";
+			this.PrintButton.UseVisualStyleBackColor = true;
+			this.PrintButton.Click += new System.EventHandler(this.PrintButton_Click);
+			// 
+			// NumbersListbox
+			// 
+			this.NumbersListbox.FormattingEnabled = true;
+			this.NumbersListbox.Location = new System.Drawing.Point(67, 91);
+			this.NumbersListbox.Name = "NumbersListbox";
+			this.NumbersListbox.Size = new System.Drawing.Size(120, 147);
+			this.NumbersListbox.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(245, 255);
+			this.Controls.Add(this.NumbersListbox);
+			this.Controls.Add(this.PrintButton);
+			this.Controls.Add(this.TimeConsumingButton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button TimeConsumingButton;
+		private System.Windows.Forms.Button PrintButton;
+		private System.Windows.Forms.ListBox NumbersListbox;
+	}
+}
+
---
+++ Windows Forms Applications/Practice/_001_Multithreading/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+namespace _001_Multithreading
+	{
+		public partial class MainForm : Form
+			{
+				public MainForm()
+					{
+						InitializeComponent();
+					}
+
+				private void TimeConsumingButton_Click(object sender, System.EventArgs e)
+					{
+						TimeConsumingButton.Enabled = false;
+						PrintButton.Enabled = false;
+						
+						Thread workerThread = new Thread(DoTimeConsumingWork);
+						workerThread.Start();
+
+						TimeConsumingButton.Enabled = true;
+						PrintButton.Enabled = true;
+					}
+
+				private void DoTimeConsumingWork()
+					{
+						Thread.Sleep(5000);
+					}
+
+				private void PrintButton_Click(object sender, System.EventArgs e)
+					{
+						for (int i = 1; i <= 10; i++)
+							{
+								NumbersListbox.Items.Add(i);
+							}
+					}
+			}
+	}
