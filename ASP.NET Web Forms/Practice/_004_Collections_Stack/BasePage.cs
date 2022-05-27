c89ba0db6456600620297bdc9c278f895f740b95
Rob C <caleror1083@gmail.com>
2022-05-27 08:24:57 -0400

    Added BasePage to Stack WebForm

---
+++ ASP.NET Web Forms/Practice/_004_Collections_Stack/BasePage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace _004_Collections_Stack
+	{
+		public class BasePage : Page
+			{
+				protected override void OnLoad(EventArgs e)  // raises the Load event - overriding the OnLoad method in System.Web.UI.Control
+					{
+						if (Session["URLStack"] == null)
+							{
+								Stack<string> urlStack = new Stack<string>();
+								Session["URLStack"] = urlStack;
+							}
+
+						if (Request.UrlReferrer != null && !Page.IsPostBack && Session["BackButtonClicked"] == null)
+							{
+								Stack<string> urlStack = (Stack<string>)Session["URLStack"];
+								urlStack.Push(Request.UrlReferrer.AbsoluteUri);
+							}
+
+						if (Session["BackButtonClicked"] != null)
+							{
+								Session["BackButtonClicked"] = null;
+							}
+					}
+			}
+	}
