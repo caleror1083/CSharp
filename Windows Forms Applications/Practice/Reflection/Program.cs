48ecc5f12df6167c401b0da2b685f4e9729b7603
Rob C <caleror1083@gmail.com>
2022-05-27 15:10:51 -0400

    No changes

--- Windows Forms Applications/Practice/Reflection/Program.cs
+++ Windows Forms Applications/Practice/Reflection/Program.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Reflection
 	{
 		internal static class Program
 			{
-				/// <summary>
-				/// The main entry point for the application.
-				/// </summary>
-				[STAThread]
 				static void Main()
 					{
 						Application.EnableVisualStyles();
--- Windows Forms Applications/Practice/UltimateSource/Program.cs
+++ Windows Forms Applications/Practice/UltimateSource/Program.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace UltimateSource
-{
-	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-		Application.EnableVisualStyles();
-		Application.SetCompatibleTextRenderingDefault(false);
-		Application.Run(new Form1());
-		}
+		static class Program
+			{
+				static void Main()
+					{
+						Application.EnableVisualStyles();
+						Application.SetCompatibleTextRenderingDefault(false);
+						Application.Run(new Form1());
+					}
+			}
 	}
-}
