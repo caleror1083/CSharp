5d1680ecb8ffcad1a10bd1d66777bd0940c605bd
Rob C <caleror1083@gmail.com>
2022-05-27 11:49:32 -0400

    Completed design and code for Master page

--- ASP.NET Web Forms/Practice/_004_Collections_Stack/Site.Master.cs
+++ ASP.NET Web Forms/Practice/_004_Collections_Stack/Site.Master.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace _004_Collections_Stack
-{
-	public partial class Site : System.Web.UI.MasterPage
 	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+		public partial class Site : MasterPage
+			{
+				protected void Page_Load(object sender, EventArgs e)
+					{
 
-		}
+					}
+
+				protected void BackButton_Click(object sender, EventArgs e)
+					{
+						Session["BackButtonClicked"] = "YES";
+
+						if (Session["URLStack"] != null)
+							{
+								Stack<string> urlStack = (Stack<string>)Session["URLStack"];
+
+								if (urlStack.Count > 0)
+									{
+										string url = urlStack.Pop();
+										Response.Redirect(url);
+									}
+								else
+									{
+										MessageLabel.Text = "There are no pages in the history";
+									}
+							}
+					}
+			}
 	}
-}
