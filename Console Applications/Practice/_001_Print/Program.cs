6923e1d76ab688993da56b49c973ea73e6099c86
Rob C <caleror1083@gmail.com>
2022-05-29 09:47:11 -0400

    No changes

--- Console Applications/Practice/_001_Print/Program.cs
+++ Console Applications/Practice/_001_Print/Program.cs
@@ -1,18 +1,12 @@
 ﻿// Robert Calero - App displaying text, escape sequences and verbatim literals
-
 namespace _001_Print
 	{
-		class Program    // Class declaration - a class defines the variables and methods used by objects
-			{
-				/* The main function is where execution begins.
-				   static methods belong to the class and can be executed without needing to create an object.
-				   void means that this method doesn't return a value after it executes, but it is common to use int instead
-				   if int is utilized it will return an integer value 0 when successfully executed or -1 when an error occurred.
-				   this function can receive multiple string values that are saved into an array */		
+		internal class Program    // Class declaration - a class defines the variables and methods used by objects
+			{	
 				static void Main()
 					{
-						Print myPrint = new Print();
-						myPrint.PrintSomething();
-					}    // end main
-			}    // end class Program
+						Examples myExamples = new Examples();
+						myExamples.ProcessExamples();
+					}
+			}
 	}
