6a34fbde1c73f93f3d5d4c9500481186b69bd1a9
Rob C <caleror1083@gmail.com>
2022-05-30 20:36:02 -0400

    Added days to the Calendar project

--- Windows Forms Applications/Practice/Calendar/MainForm.cs
+++ Windows Forms Applications/Practice/Calendar/MainForm.cs
@@ -10,9 +10,31 @@ namespace Calendar
 						InitializeComponent();
 					}
 
-				private void MainForm_Load(object sender, EventArgs e)
+				void MainForm_Load(object sender, EventArgs e)
 					{
+						DisplayDays();	
+					}
 
+				void DisplayDays()
+					{
+						DateTime startoftheMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);  // // Gets the first day of the month
+						int days = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month); // Gets the count of days of the month
+						int dayoftheweek = Convert.ToInt32(startoftheMonth.DayOfWeek.ToString("D")) + 1;  // Convert the startofthemonth to integer
+
+						// Iterate through the days of the week adding a blank user control in each iteration
+						for (int i = 1; i < dayoftheweek; i++)
+							{
+								UserControlBlank control = new UserControlBlank();
+								DayContainer.Controls.Add(control);
+							}
+
+						// Iterate through the days in the month adding a days user control in each iteration
+						for (int i = 1; i <= days; i++)
+							{
+								UserControlDays controlDays = new UserControlDays();
+								controlDays.Days(i);
+								DayContainer.Controls.Add(controlDays);
+							}
 					}
 			}
 	}
---
+++ Windows Forms Applications/Practice/Calendar/UserControlDays.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Calendar
+{
+	partial class UserControlDays
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.DaysLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// DaysLabel
+			// 
+			this.DaysLabel.AutoSize = true;
+			this.DaysLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.DaysLabel.Location = new System.Drawing.Point(14, 15);
+			this.DaysLabel.Name = "DaysLabel";
+			this.DaysLabel.Size = new System.Drawing.Size(27, 20);
+			this.DaysLabel.TabIndex = 0;
+			this.DaysLabel.Text = "00";
+			// 
+			// UserControlDays
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.Controls.Add(this.DaysLabel);
+			this.Name = "UserControlDays";
+			this.Size = new System.Drawing.Size(150, 108);
+			this.Load += new System.EventHandler(this.UserControlDays_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label DaysLabel;
+	}
+}
