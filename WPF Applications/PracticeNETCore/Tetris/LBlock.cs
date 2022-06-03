170c518908151ec0013d97b5611a9507de0eff1c
Rob C <caleror1083@gmail.com>
2022-06-03 19:51:29 -0400

    Completed LBlock for Tetris

---
+++ WPF Applications/PracticeNETCore/Tetris/LBlock.cs
@@ -0,0 +1,22 @@
+﻿// Namespaces
+namespace Tetris
+	{
+		// Classes
+		internal class LBlock : Block
+			{
+				// Fields
+				// Store the tiles positions for the four rotation states
+				private readonly Position[][] _Tiles = new Position[][]  
+					{
+						new Position[] { new(0, 2), new(1, 0), new(1, 1), new(1, 2) },
+						new Position[] { new(0, 1), new(1, 1), new(2, 1), new(2, 2) },
+						new Position[] { new(1, 0), new(1, 1), new(1, 2), new(2, 0) },
+						new Position[] { new(0, 0), new(0, 1), new(1, 1), new(2, 1) }
+					};
+
+				// Properties
+				public override int Id => 3;
+				protected override Position StartOffset => new(0, 3); // Makes the block spawn in the middle of the top row
+				protected override Position[][] Tiles => _Tiles;  // Return the _Tiles array field
+			}
+	}
