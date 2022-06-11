35a5d9fe3a5b701bbba2240f40674176ae7f0508
Rob C <caleror1083@gmail.com>
2022-06-11 04:06:28 -0400

    No changes

--- Console Applications/Practice/_005_CharacterDataType/Examples.cs
+++ Console Applications/Practice/_005_CharacterDataType/Examples.cs
@@ -75,6 +75,16 @@ namespace _005_CharacterDataType
 
 				static void Example3()
 					{
+						/* Write a program that prints an isosceles triangle of 9 copyright symbols ©, something like this:
+						      ©
+							 © ©
+							©   ©
+						   © © © ©
+						 * Note that the © symbol may be displayed incorrectly at the console so you may need to change the 
+                         * console character encoding to UTF-8 and assign a Unicode-friendly font in the console. Note also, 
+                         * that under old versions of Windows the © symbol may still be displayed incorrectly, regardless of 
+                         * how much effort you put to fix it. */
+
 						Console.OutputEncoding = Encoding.UTF8;
 						char copyRight = '\u00A9';
 
