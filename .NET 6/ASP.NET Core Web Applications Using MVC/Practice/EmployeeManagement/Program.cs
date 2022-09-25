342d3f278d94e78017553b11acfc706e90d278e2
Rob C <caleror1083@gmail.com>
2022-09-24 21:03:08 -0400

    No changes

--- .NET 6/ASP.NET Core Web Applications Using MVC/Practice/EmployeeManagement/Program.cs
+++ .NET 6/ASP.NET Core Web Applications Using MVC/Practice/EmployeeManagement/Program.cs
@@ -1,19 +1,4 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace EmployeeManagement
 	{
@@ -24,7 +9,7 @@ namespace EmployeeManagement
 						WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 						WebApplication app = builder.Build();
 
-						app.MapGet("/", () => "Hello World!");
+						app.MapGet("/", () => "");
 
 						app.Run();
 					}
