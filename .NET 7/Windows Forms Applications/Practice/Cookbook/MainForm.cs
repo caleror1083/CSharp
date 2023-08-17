1bd95f71660cfd19e1423e6c905ac8fb370c1082
Rob C <caleror1083@gmail.com>
2023-08-17 04:36:31 -0400

    Updated Cookbook project to .NET 7

--- .NET 7/Windows Forms Applications/Practice/Cookbook/MainForm.cs
+++ .NET 7/Windows Forms Applications/Practice/Cookbook/MainForm.cs
@@ -1,20 +1,23 @@
-﻿using Cookbook.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
+﻿using Microsoft.Data.SqlClient;
 using System;
-using System.Configuration;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Cookbook;
 
 public partial class MainForm : Form
 {
+    private SqlConnection connection;  // Provides the connection to the database
+	private SqlDataAdapter dataAdapter;
+	private SqlCommand command;  // Another form to handle SELECT queries and support parameters
+	private readonly string connectionString;
+
     public MainForm()
     {
         InitializeComponent();
+
+		// When the form is created, initialize the connectionString to the database
+		connectionString = Properties.Settings.Default.connectionString;
     }
 
     private void MainForm_Load(object sender, EventArgs e)
@@ -23,85 +26,115 @@ public partial class MainForm : Form
             PopulateRecipes();
     }
 
-	private static CookbookContext GetContext()
-	{
-         return new CookbookContext();
-	}
-
     private void PopulateCounterIngredients()
     {
-        // read all the ingredients from the database and display them in the listbox
-        using var context = GetContext();
-        CounterIngredientsListbox.DisplayMember = "Name";
-        CounterIngredientsListbox.ValueMember = "Id";
-        CounterIngredientsListbox.DataSource = context.Ingredients.ToList();
+        string counterQuery = $"SELECT * FROM Ingredient";
+
+        using (connection = new SqlConnection(connectionString))
+        using (dataAdapter = new SqlDataAdapter(counterQuery, connection))
+        {
+            var counterTable = new DataTable();
+            dataAdapter.Fill(counterTable);
+            CounterIngredientsListbox.DisplayMember = "Name";
+            CounterIngredientsListbox.ValueMember = "Id";
+            CounterIngredientsListbox.DataSource = counterTable;
+        }
     }
 
-	void PopulateRecipes()
+	private void PopulateRecipes()
     {
-        // read all the recipes from the database and display them in the listbox
-        using var context = GetContext();
-        RecipesListbox.DisplayMember = "Name";
-        RecipesListbox.ValueMember = "Id";
-        RecipesListbox.DataSource = context.Recipes.ToList();
+        string recipeQuery = $"SELECT * FROM Recipe";
+
+        // Use a using statement with an object like SqlConnection because it implements IDosposable it auto closes the object
+        using (connection = new SqlConnection(connectionString))  // initialize the connection
+        using (dataAdapter = new SqlDataAdapter(recipeQuery, connection))  // SqlDataAdapter also implements IDisposable and also opens the connection
+        {
+            var recipeTable = new DataTable(); // Holds the data returned by the SQL query
+            dataAdapter.Fill(recipeTable);  // Fills the table with the data in the DataTable
+            RecipesListbox.DisplayMember = "Name";  // Displays the value of Name
+            RecipesListbox.ValueMember = "Id";  // Reference the value by the ID column
+            RecipesListbox.DataSource = recipeTable;  // Hook up listbox reciped to recipe DataTable
+        }
     }
 
-	void AddRecipeButton_Click(object sender, EventArgs e)
-		{
-			// insert into the recipe table the value of the recipeName parameter
-			using var context = GetContext();
-			var recipe = new Recipe
-            {
-                Name = RecipeNameTextbox.Text,
-				PrepTime = 80,
-				Instructions = "Many Instructions"
-            };
-			context.Recipes.Add(recipe);
-			context.SaveChanges();
-			PopulateRecipes();
-		}
+    private void AddRecipeButton_Click(object sender, EventArgs e)
+    {
+        var addRecipeQuery = $"INSERT INTO Recipe " +
+                             $"VALUES (@RecipeName, 80, 'Many instructions')";
 
-	void AddIngredientButton_Click(object sender, EventArgs e)
-		{
-			// insert into the recipeingredient table the value of the recipeId and ingredientId parameters
-			using var context = GetContext();
-			var recipeIngredient = new RecipeIngredient
-            {
-                RecipeId = (int)RecipesListbox.SelectedValue,
-                IngredientId = (int)CounterIngredientsListbox.SelectedValue
-            };
-			context.RecipeIngredients.Add(recipeIngredient);
-			context.SaveChanges();
-			PopulateRecipes();
-		}
+        using (connection = new SqlConnection(connectionString))
+        using (command = new SqlCommand(addRecipeQuery, connection))
+        {
+            connection.Open();  // When no DataAdapter is called, you must open the connection manually
+            command.Parameters.AddWithValue("@RecipeName", RecipeNameTextbox.Text);
+            command.ExecuteNonQuery();
+        }
 
-	void UpdateRecipeButton_Click(object sender, EventArgs e)
-		{
-			// update the recipe table with the value of the recipe name textbox where the recipe id matches the selected recipe id in the listbox
-			using var context = GetContext();
-			var recipe = context.Recipes.Find(RecipesListbox.SelectedValue);
-			recipe.Name = RecipeNameTextbox.Text;
-			context.SaveChanges();
-			PopulateRecipes();
-		}
+        PopulateRecipes();
+    }
 
-	void RecipesListbox_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			PopulateIngredients();
-		}
+    private void AddIngredientButton_Click(object sender, EventArgs e)
+    {
+        var addRecipeQuery = $"INSERT INTO RecipeIngredient " +
+                             $"VALUES (@RecipeId, @IngredientId)";
 
-	void PopulateIngredients()
-		{
-			// read all the ingredients from the database and display them in the listbox where the recipe id matches the selected recipe id in the listbox
-			using var context = GetContext();
-			IngredientsListbox.DisplayMember = "Name";
-			IngredientsListbox.ValueMember = "Id";
-				
-			var recipeId = (int)RecipesListbox.SelectedValue;
-			var ingredients = context.RecipeIngredients
-                .Where(ri => ri.RecipeId == recipeId)
-                .Select(ri => ri.Ingredient)
-                .ToList();
-				IngredientsListbox.DataSource = ingredients;
-		}
+        using (connection = new SqlConnection(connectionString))
+        using (command = new SqlCommand(addRecipeQuery, connection))
+        {
+            connection.Open();
+
+            // Whatever recipe is selected in the listbox
+            // get the value of it and pass it into the ingredientQuery which will return the ingredients that are tied to that recipe
+            command.Parameters.AddWithValue("@RecipeId", RecipesListbox.SelectedValue);
+            command.Parameters.AddWithValue("@IngredientId", CounterIngredientsListbox.SelectedValue);
+            command.ExecuteNonQuery();
+        }
+
+        PopulateRecipes();
+    }
+
+    private void UpdateRecipeButton_Click(object sender, EventArgs e)
+    {
+        var updateRecipeQuery = $"UPDATE Recipe " +
+                                $"SET Name = @RecipeName " +
+                                $"WHERE Id = @RecipeId";
+
+        using (connection = new SqlConnection(connectionString))
+        using (command = new SqlCommand(updateRecipeQuery, connection))
+        {
+            connection.Open();
+            command.Parameters.AddWithValue("@RecipeName", RecipeNameTextbox.Text);
+            command.Parameters.AddWithValue("@RecipeId", RecipesListbox.SelectedValue);
+            command.ExecuteNonQuery();
+        }
+
+        PopulateRecipes();
+    }
+
+    private void RecipesListbox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        PopulateIngredients();
+    }
+
+    private void PopulateIngredients()
+    {
+        // @RecipeId is syntax for parameter
+        var ingredientQuery = $"SELECT a.Name " +
+                              $"FROM Ingredient a " +
+                              $"INNER JOIN RecipeIngredient b ON a.Id = b.IngredientId " +
+                              $"WHERE b.RecipeId = @RecipeId";
+
+        using (connection = new SqlConnection(connectionString))
+        using (command = new SqlCommand(ingredientQuery, connection))
+        using (dataAdapter = new SqlDataAdapter(command))
+        {
+            // Whatever recipe is selected in the listbox, get the value of it and pass it into the ingredientQuery which will return the ingredients that are tied to that recipe
+            command.Parameters.AddWithValue("@RecipeId", RecipesListbox.SelectedValue);
+            var ingredientTable = new DataTable();
+            dataAdapter.Fill(ingredientTable);
+            IngredientsListbox.DisplayMember = "Name";
+            IngredientsListbox.ValueMember = "Id";
+            IngredientsListbox.DataSource = ingredientTable;
+        }
+    }
 }
