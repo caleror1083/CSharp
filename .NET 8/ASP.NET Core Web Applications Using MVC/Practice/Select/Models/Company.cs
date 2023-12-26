075101725a1bdb9c967dd401e2f47eec91b28f8c
Rob C <caleror1083@gmail.com>
2023-12-26 17:56:27 -0500

    Updated more apps

---
+++ .NET 8/ASP.NET Core Web Applications Using MVC/Practice/Select/Models/Company.cs
@@ -0,0 +1,51 @@
+﻿namespace Select.Models;
+
+public class Company
+{
+    public List<Employee>? EmployeeList { get; set; }
+    
+    public string this[int employeeID] // indexer
+    {
+        get
+        {
+            return EmployeeList.FirstOrDefault(employee => employee.EmployeeID == employeeID).Name;
+        }
+        set
+        {
+            EmployeeList.FirstOrDefault(employee => employee.EmployeeID == employeeID).Name = value;
+        }
+    }
+
+    public string this[string gender] // overloading indexer
+    {
+        get
+        {
+            return EmployeeList.Count(employee => employee.Gender == gender).ToString();
+        }
+        set
+        {
+            foreach (Employee employee in EmployeeList)
+            {
+                if (employee.Gender == gender)
+                {
+                    employee.Gender = value;
+                }
+            }
+        }
+    }
+
+    public Company()
+    {
+        EmployeeList =
+        [
+            new Employee() { EmployeeID = 1, Name = "Mike", Gender = "Male" },
+            new Employee() { EmployeeID = 2, Name = "Pam", Gender = "Female" },
+            new Employee() { EmployeeID = 3, Name = "John", Gender = "Male" },
+            new Employee() { EmployeeID = 4, Name = "Maxine", Gender = "Female" },
+            new Employee() { EmployeeID = 5, Name = "Emily", Gender = "Female" },
+            new Employee() { EmployeeID = 6, Name = "Scott", Gender = "Male" },
+            new Employee() { EmployeeID = 7, Name = "Todd", Gender = "Male" },
+            new Employee() { EmployeeID = 8, Name = "Ben", Gender = "Male" },
+        ];
+    }
+}
--- .NET Framework 4.8.1/ASP.NET Web Forms/Practice/_002_Indexers/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_002_Indexers.Global" Language="C#" %>
