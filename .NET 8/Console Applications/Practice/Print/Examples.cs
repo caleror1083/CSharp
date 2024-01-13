402c83238dcfadb6003678ad7e1ec889ac0abeea
Rob C <caleror1083@gmail.com>
2024-01-13 08:12:36 -0500

    Updated Print to .NET 8

---
+++ .NET 8/Console Applications/Practice/Print/Examples.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Print;
+
+internal class Examples
+{
+    public static void ProcessExamples()
+  	{
+    		Start:
+			      Console.Write($"Please select an example from 1-16: ");
+			      bool check = int.TryParse(Console.ReadLine(), out int choice);
+
+			      if (check)
+			      {
+				        switch (choice)
+				        {
+					          case 1:
+						            Example1();
+						            break;
+					          case 2:
+						            Example2();
+						            break;
+					          case 3:
+						            Example3();
+						            break;
+					          case 4:
+						            Example4();
+						            break;
+					          case 5:
+						            Example5();
+						            break;
+					          case 6:
+						          Example6();
+						          break;
+					          case 7:
+						            Example7();
+						            break;
+					          case 8:
+						            Example8();
+						            break;
+					          case 9:
+						            Example9();
+						            break;
+					          case 10:
+						            Example10();
+						            break;
+					          case 11:
+						            Example11();
+						            break;
+					          case 12:
+						            Example12();
+						            break;
+					          case 13:
+						            Example13();
+						            break;
+					          case 14:
+						            Example14();
+						            break;
+					          case 15:
+						            Example15();
+						            break;
+					          case 16:
+						            Example16();
+						            break;
+					          default:
+						            Console.WriteLine($"Your choice is invalid");
+						            goto Start;
+				        }
+			      }
+			      else
+			      {
+				        Console.WriteLine($"Selection chosen is incorrect");
+				        goto Start;
+			      }
+
+		    Decide:
+			      Console.Write($"Do you want to do another example? - Yes or No: ");
+			      string decision = Console.ReadLine();
+			      Console.WriteLine();
+
+			      switch (decision.ToUpper())
+			      {
+				        case "YES":
+					          goto Start;
+				        case "NO":
+					          break;
+				        default:
+					          Console.WriteLine($"Your decision is invalid. Try again");
+					          goto Decide;
+			      }
+	  }
+
+    // alternate way to go to newline
+    private static void Example1() => Console.Write($"Writes a string to the console followed by a newline escape sequence\n");
+    private static void Example2() => Console.WriteLine();  // displays blank line, goes to new line
+    private static void Example3()
+	  {
+		    Console.WriteLine($"Hello, C#");  // a statement
+	  }
+
+	  private static void Example4()
+	  {
+		    Console.WriteLine($"Displays \' in a string");  // display apostrophes in a string
+	  }
+
+	  private static void Example5()
+	  {
+		    Console.WriteLine($"Displays \" in a string");  // displays double quotes in a string
+	  }
+
+	  private static void Example6()
+	  {
+        // verbatim string literal
+		    Console.WriteLine(@"Displays "" in a string without using an escape sequence instead using verbatim string literal");
+	  }
+
+	  private static void Example7()
+	  {
+		    Console.WriteLine($"Makes \abell alert chime");  // makes bell alert chime
+	  }
+
+	  private static void Example8()
+	  {
+		    Console.WriteLine($"Displays a \t horizontal tab in a string");  // displays a 4 space horizontal tab in a string
+	  }
+
+	  private static void Example9()
+	  {
+		    Console.WriteLine($"Displays a \v vertical tab in a string");  // displays vertical tabs in a string
+	  }
+
+	  private static void Example10()
+	  {
+		    Console.WriteLine($"Displays \\ in a string");  // displays a forward slash in a string
+	  }
+
+	  private static void Example11()
+	  {
+        // displays \ in a string using a verbatim string literal
+		    Console.WriteLine(@"Displays \ in a string without using an escape sequence instead using verbatim string literal");
+	  }
+
+	  private static void Example12()
+	  {
+		    Console.Write($"This is a carriage return\r");  // carriage return
+	  }
+
+	  private static void Example13()
+	  {
+		    Console.Write($"Something");  // print without a newline
+		    Console.WriteLine(" else");
+	  }
+
+	  private static void Example14()
+	  {
+        // display 1 line of text in multiple lines of code
+		    Console.WriteLine($"Something " +
+					                $"else");       
+	  }
+
+	  private static void Example15()
+	  {	
+		    // write a program to print the numbers 1, 101 and 1001, each in a separate line
+		    Console.WriteLine(1);
+		    Console.WriteLine(101);
+		    Console.WriteLine(1001);
+	  }
+
+	  private static void Example16() => Console.WriteLine("testing");  // using a lambda operator goes to print to the console
+}
