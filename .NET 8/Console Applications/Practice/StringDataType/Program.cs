2a1fc269bffbfd4d65ddc9488c41449252728614
Rob C <caleror1083@gmail.com>
2024-01-15 05:38:37 -0500

    Updated StringDataType to .NET 8

---
+++ .NET 8/Console Applications/Practice/StringDataType/Program.cs
@@ -0,0 +1,11 @@
+﻿// Robert Calero - App displaying String data type declaration, initialization, printing variable value to the console, String methods
+
+namespace StringDataType;
+
+internal class Program
+{
+    private static void Main()
+	{
+        Examples.ProcessExamples();
+	}
+}	
