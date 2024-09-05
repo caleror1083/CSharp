6dc5a78060ed373551092c773cb23326ad64c47a
Rob C <caleror1083@gmail.com>
2024-09-05 05:51:01 -0400

    Added exit button control to login form

--- .NET 8/Windows Forms Applications/Practice/DentalOfficeSchedulingApp/Login.Designer.cs
+++ .NET 8/Windows Forms Applications/Practice/DentalOfficeSchedulingApp/Login.Designer.cs
@@ -7,6 +7,7 @@ partial class Login
 {
   private IContainer components = null;
   private Button LoginBtn;
+  private Button ExitBtn;
 
   protected override void Dispose(bool disposing)
 	{
@@ -23,6 +24,7 @@ partial class Login
   {
     ComponentResourceManager resources = new ComponentResourceManager(typeof(Login));
     LoginBtn = new Button();
+    ExitBtn = new Button();
     SuspendLayout();
     // 
     // LoginBtn
@@ -31,10 +33,17 @@ partial class Login
     LoginBtn.Name = "LoginBtn";
     LoginBtn.UseVisualStyleBackColor = true;
     // 
+    // ExitBtn
+    // 
+    resources.ApplyResources(ExitBtn, "ExitBtn");
+    ExitBtn.Name = "ExitBtn";
+    ExitBtn.UseVisualStyleBackColor = true;
+    // 
     // Login
     // 
     resources.ApplyResources(this, "$this");
     AutoScaleMode = AutoScaleMode.Font;
+    Controls.Add(ExitBtn);
     Controls.Add(LoginBtn);
     Name = "Login";
     ResumeLayout(false);
