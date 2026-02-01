c7d21228429f8381641618f1f9e09bee0692c338
Rob C <caleror1083@gmail.com>
2026-02-01 10:45:01 -0500

    More updates

---
+++ .NET 10/Console Applications/Practice/TicTacToe/GameVisualizer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TicTacToe;
+
+class GameVisualizer
+	{
+		public static void PrintBoard(string[,] argTicTacToeBoard)
+			{
+				for (int i = 0; i < argTicTacToeBoard.GetLength(0); i++)
+					{
+						for (int j = 0; j < argTicTacToeBoard.GetLength(0); j++)
+							{
+								Console.Write(argTicTacToeBoard[i, j]);
+							}
+						Console.WriteLine();
+					}
+			}
+
+		public static void InitializeBoard(string[,] argTicTacToeBoard)
+			{
+				for (int i = 0; i < argTicTacToeBoard.GetLength(0); i++)
+					{
+						for (int j = 0; j < argTicTacToeBoard.GetLength(0); j++)
+							{
+								argTicTacToeBoard[i, j] = " ";
+							}
+					}
+			}				
+	}
---
+++ .NET 10/Console Applications/Practice/TicTacToe/Program.cs
@@ -0,0 +1,10 @@
+﻿// Robert Calero - App displaying Tic Tac Toe Game
+namespace TicTacToe;
+
+class Program    // Class declaration
+	{
+		static void Main()    // Main method begins execution of C# app
+			{
+				GameUI.GameBoard();
+			}
+	}
